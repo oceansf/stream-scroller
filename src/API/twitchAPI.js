330a4ff27029867f6c8552ac7d88ab5e55d59ffe
Ocean Fuaga <oceansfuaga@gmail.com>
2020-11-09 19:01:59 -0800

    hard code client id to fix fetchStreams

--- src/API/twitchAPI.js
+++ src/API/twitchAPI.js
@@ -3,16 +3,16 @@ import axios from 'axios';
 export const authURL = `https://id.twitch.tv/oauth2/authorize?client_id=c7dav85rh66sf1oxxiipu41acyatil&redirect_uri=https://practical-villani-7ae8dc.netlify.app&response_type=token`;
 
 export const fetchStreams = async () => {
-  const authURI = document.location.hash;
-  const authToken = authURI.substring(14, 44);
-  const result = await axios.get(
-    'https://api.twitch.tv/helix/streams?first=100&language=en',
-    {
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-        'Client-Id': `${process.env.REACT_APP_CLIENT_ID}`,
-      },
-    }
-  );
-  return result.data.data;
+	const authURI = document.location.hash;
+	const authToken = authURI.substring(14, 44);
+	const result = await axios.get(
+		'https://api.twitch.tv/helix/streams?first=100&language=en',
+		{
+			headers: {
+				Authorization: `Bearer ${authToken}`,
+				'Client-Id': 'c7dav85rh66sf1oxxiipu41acyatil',
+			},
+		}
+	);
+	return result.data.data;
 };
